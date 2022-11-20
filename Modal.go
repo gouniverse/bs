12eb561d9e0cf9a35e4551039a2b6b6e93d73acd
Milan Lesichkov <lesichkovm@gmail.com>
2022-11-20 23:32:00 +0000

    Added modal

---
+++ Modal.go
@@ -0,0 +1,25 @@
+package bs
+
+import (
+	"github.com/gouniverse/hb"
+)
+
+func Modal() *hb.Tag {
+	return hb.NewDiv().Class("modal")
+}
+
+func ModalDialog() *hb.Tag {
+	return hb.NewDiv().Class("modal-dialog")
+}
+
+func ModalHeader() *hb.Tag {
+	return hb.NewDiv().Class("modal-header")
+}
+
+func ModalBody() *hb.Tag {
+	return hb.NewDiv().Class("modal-body")
+}
+
+func ModalFooter() *hb.Tag {
+	return hb.NewDiv().Class("modal-footer")
+}
