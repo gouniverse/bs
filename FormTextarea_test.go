d632c3fac752c25f18866485ccad0251b9be28e0
Milan Lesichkov <lesichkovm@gmail.com>
2023-04-12 00:43:38 +0100

    Update FormTextarea_test.go

--- FormTextarea_test.go
+++ FormTextarea_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestFormTextArea(t *testing.T) {
-	nav := FormTextarea().ToHTML()
+	nav := FormTextArea().ToHTML()
 	if nav != "<textarea class=\"form-control\"></textarea>" {
 		t.Log("Expected: ", `<textarea class="form-control"></textarea>`, " but found:", nav)
 		t.Fail()
